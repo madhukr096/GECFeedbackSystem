cf39f4e5c5b765b0e88537279cdd1778eca8f988
madhukr096 <77736214+madhukr096@users.noreply.github.com>
2021-01-20 20:29:21 +0530

    Add files via upload

---
+++ admin/feedback_average1.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Dashboard</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../css/metisMenu.min.css" rel="stylesheet">
+
+    
+    <!-- Custom CSS -->
+    <link href="../css/sb-admin-2.css" rel="stylesheet">
+
+
+    <!-- Custom Fonts -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<img src="..//images/Downloads_0_0.jpg" height="127" width="1364" >
+  <!-- Navigation -->
+    <nav class="navbar navbar-default navbar" role="navigation" style="background:#0366fc;padding: 10px;font-weight: bold;font-size: 16px">
+        <div class="container" >
+		
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+              <a class="navbar-brand" href="index.php" style="color:#FFFFFF">Admin Dashboard</a>
+				
+			
+			
+            </div>  
+</div>
+</nav>	
+</nav>	
+	<div id="wrapper">
+
+    <div style="margin-top: 120px">
+
+    <span style="margin-left: 40px"> <a href="dashboard.php?info=add_faculty"> <input type="button" value="Add Faculty" style="width: 160px;height: 50px;background-color: #fc0366;color: white;border-radius: 5px;font-weight: bold" ></a></span>
+
+
+    <span style="margin-left: 45px"> <a href="dashboard.php?info=show_faculty"> <input type="button" value="Manage Faculty" style="width: 160px;height: 50px;background-color: #fc0366;color: white;border-radius: 5px;font-weight: bold" ></a></span>
+
+    <span style="margin-left: 50px"> <a href="dashboard.php?info=display_student"> <input type="button" value="Manage Student" style="width: 160px;height: 50px;background-color: #fc0366;color: white;border-radius: 5px;font-weight: bold" ></a></span>
+
+<span style="margin-left: 55px"> <a href="dashboard.php?info=feedback"> <input type="button" value="Manage Feedback" style="width: 160px;height: 50px;background-color: #fc0366;color: white;border-radius: 5px;font-weight: bold" ></a></span>
+
+
+    <span style="margin-left: 60px"> <a href="dashboard.php?info=contact"> <input type="button" value="Contact Page" style="width: 160px;height: 50px;background-color: #fc0366;color: white;border-radius: 5px;font-weight: bold" ></a></span>
+
+     <span style="margin-left: 65px"> <a href="logout.php"> <input type="button" value="Logout" style="width: 160px;height: 50px;background-color: green;color: white;border-radius: 5px;font-weight: bold" ></a></span>
+
+
+</div>
+   <head>
+      <title>HTML Iframes</title>
+   </head>
+	
+   <body>
+      <div>
+      
+      <iframe src = "try/try1.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div>
+      <div>
+      
+      <iframe src = "try/try2.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div>
+       <div>
+      
+      <iframe src = "try/try3.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try4.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try5.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try6.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try7.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try8.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try9.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try10.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try11.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div><div>
+      
+      <iframe src = "try/try12.php" width = "920" height = "605">
+         Sorry your browser does not support inline frames.
+      </iframe>
+      </div>
+   </body>
+	
+</html>
