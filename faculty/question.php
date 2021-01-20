dd75d8d41140f097b52046c85d91a2aa8b9dbe8b
madhukr096 <77736214+madhukr096@users.noreply.github.com>
2021-01-20 20:33:48 +0530

    Add files via upload

---
+++ faculty/question.php
@@ -0,0 +1,83 @@
+<?php 
+extract($_POST);
+?>
+<form method="post">
+<fieldset>
+<center><u>Questions</u></center><br>
+ 
+<fieldset>
+
+
+
+<h3>Please give your answer about the following question by circling the given grade on the scale:</h3>
+
+
+<button type="button" style="font-size:7pt;color:white;background-color:green;border:2px solid #336600;padding:3px">Strongly Agree 5</button>
+<button type="button" style="font-size:7pt;color:white;background-color:Brown;border:2px solid #336600;padding:3px">Agree 4</button>
+<button type="button" style="font-size:7pt;color:white;background-color:blue;border:2px solid #336600;padding:3px">Neutral 3</button>
+<button type="button" style="font-size:7pt;color:white;background-color:Black;border:2px solid #336600;padding:3px"> Disagree 2</button>
+<button type="button" style="font-size:7pt;color:white;background-color:red;border:2px solid #336600;padding:3px">Strongly Disagree 1</button><br>
+
+
+
+<h3>1-Course Material</h3>
+
+<tr>
+<td><b>1:</b> Teacher provided the course outline having weekly content plan with list of  required text book:</td>  
+
+<br>
+<td><b>2:</b>Course objectives,learning outcomes and grading criteria are clear to me:</td> 
+<br>
+
+<b>3:</b>Course integrates throretical course concepts with the real world examples:</td> 
+<br>
+
+<h3>2-Class Teaching</h3>
+<td><b>4:</b> Teacher is punctual,arrives on time and leaves on time:</td>
+<br>
+<tr>
+<td>
+<b>5:</b> Teacher is good at stimulating the interest in the course content:</td>
+<br>
+<td><b>6:</b> Teacher is good at explaining the subject matter:</td>
+
+<br>
+<tr><td>
+<b>7:</b> Teacher's presentation was clear,loud ad easy to understand:</td>
+<br>
+<b>8:</b> Teacher is good at using innovative teaching methods/ways:</td><td> 
+<br>
+<b>9:</b> Teacher is available and helpful during counseling hours:</td> 
+<br>
+<b>10:</b> Teacher has competed the whole course as per course outline:</td>
+<br>
+
+<h3>3-Class Assessment</h3>
+
+<tr>
+<td><b>11:</b>Teacher was always fair and impartial:</td>
+<br>
+<td><b>12:</b>Assessments conducted are clearly connected to maximize learining objectives:</td>
+
+<br>
+<b>13:</b>What I liked about the course:<br>
+<b>14:</b>Why I disliked about the course:<br><br>
+
+</textarea></center>
+</table>
+</form>
+</fieldset>
+
+
+<!--<a href="transport.html"><p align="right"><button type="Button"style="font-size:7pt;color:white;background-color:green;border:2px solid #336600;padding:7px">Next</button></p></a>
+<a href="About.php"><p align="right"><button type="Button" style="font-size:7pt;color:white;background-color:green;border:2px solid #336600;padding:7px">Back</button></p></a>-->
+
+</div><!--close content_item-->
+      </div><!--close content-->   
+	
+	</div><!--close site_content-->  	
+  
+    
+    </div><!--close main-->
+  </form>
+<center>
